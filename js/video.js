0044d29cf752fbde01d04884b333d7f994c3381b
Ashley Suh <adsuh@umich.edu>
2025-11-16 00:43:08 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,18 +1,3 @@
-// var video;
-
-// window.addEventListener("load", function() {
-// 	console.log("Good job opening the window")
-// 	video = this.document.querySelector("#player1")
-// video.autoplay=false;
-// console.log("Autoplay is set to " + video.autoplay);
-// video.loop=false;
-// console.log("Loop is set to " + video.loop);
-// });
-
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
 var video;
 
 window.addEventListener("load", function() {
